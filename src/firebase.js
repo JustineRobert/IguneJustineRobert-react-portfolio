9ced5f2cc4932787eda0e1088fa68ac06b180d59
Igune Justine Robert <igunejustinerobert@gmail.com>
2020-10-07 16:32:24 +0300

    Final Refined Portfolio for Justine Robert

---
+++ src/firebase.js
@@ -0,0 +1,11 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyD92P-tou7mQDTfwSB69mu_oIhiw6Dnesk",
+  authDomain: "justine-portfolio-54edd.firebaseapp.com",
+  databaseURL: "https://justine-portfolio-54edd.firebaseio.com",
+  projectId: "justine-portfolio-54edd",
+  storageBucket: "justine-portfolio-54edd.appspot.com",
+  messagingSenderId: "506396692414",
+  appId: "1:506396692414:web:1b0ba42af23732ac80b3ef",
+  measurementId: "G-B5ZZH0JB4L"
+};
